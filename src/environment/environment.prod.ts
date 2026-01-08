0742dab3d61c3f062f946f1e422aea2a2883dc12
devswapnil1995 <developer.swapnilbhoir@gmail.com>
2026-01-08 15:12:21 +0530

    Added free .net api testing

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://realtymind-api-etc3esb8fgdwe2ey.centralindia-01.azurewebsites.net', // later
+  apiBaseUrl: 'http://realtymind.runasp.net',//'https://realtymind-api-etc3esb8fgdwe2ey.centralindia-01.azurewebsites.net',
   googleMapsApiKey: "AIzaSyAMoj0cTbbIqdC_HPon1Rrz0mlVdh6LdGs"
 };
