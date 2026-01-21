c07c60d29c9b0037c85e2452851341977eadb8a4
devswapnil1995 <developer.swapnilbhoir@gmail.com>
2026-01-21 19:44:47 +0530

    Added secure call

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://realtymind.runasp.net',//'https://realtymind-api-etc3esb8fgdwe2ey.centralindia-01.azurewebsites.net',
+  apiBaseUrl: 'https://realtymind.runasp.net',//'https://realtymind-api-etc3esb8fgdwe2ey.centralindia-01.azurewebsites.net',
   googleMapsApiKey: "AIzaSyAMoj0cTbbIqdC_HPon1Rrz0mlVdh6LdGs"
 };
